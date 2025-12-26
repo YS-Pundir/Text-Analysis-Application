404186b427a3388ffbe81a704c87db01bfdbaefe
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-12-26 02:53:43 +0100

    adding Textfile.txt

---
+++ code.py
@@ -0,0 +1,33 @@
+
+class text_analysis:
+    def __init__(self):
+        self.text=""
+
+    def text_input(self):
+        print("-"*15,"Text analysis Application","*"*15)
+        print()
+        print("Please choose one option :- \nOption A : input as a Text\nOption B : input from a file ")
+        choice=input("Enter your choice :-",)
+
+        if choice=="A": 
+            line=[]
+            print("Enter text (press Enter twice to exit ) :- ")
+            while True:
+                word=input()
+                line.append(word)
+                if word=="":
+                    break
+            self.text=" ".join(line)
+
+        elif choice=="B":
+            name=input("Enter the name of the file :- ",)
+            try:
+                with open(name,"r") as file:
+                    self.text=file.read()
+            except :
+                print("file not found")
+        print(self.text)
+
+
+c1=text_analysis()
+c1.text_input()
